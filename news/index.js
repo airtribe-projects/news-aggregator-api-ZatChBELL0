675ec9e895fc1922f8c17a62312b09f2350860b9
Danish <danishakhtar242@gmail.com>
2026-02-02 22:38:27 +0530

    project submission

---
+++ news/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { authMiddleware } = require("../middlewares/authMiddleware");
+const { default: axios } = require("axios");
+
+const router = express.Router();
+
+router.get("/", authMiddleware, async (req, res) => {
+  const preferences = req.user?.preferences || [];
+
+  if (!Array.isArray(preferences) || preferences.length === 0) {
+    return res.status(400).json({ message: "User preferences not set." });
+  }
+
+  const stringifiedPreferences = preferences.join(" OR ");
+
+  const news = [];
+
+  try {
+    const response = await axios.get(
+      "https://gnews.io/api/v4/search",
+      {
+        params: {
+          q: stringifiedPreferences,
+          lang: "en",
+          country: "us",
+          max: 10,
+          token: process.env.NEWS_API_KEY,
+        },
+      },
+    );
+    news.push(...response.data.articles);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    return res.status(500).json({ message: "Error fetching news." });
+  }
+
+  return res.status(200).json({ news });
+});
+
+module.exports = { newsRoutes: router };
